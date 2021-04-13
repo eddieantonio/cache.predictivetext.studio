d276952cc79efb21e2bbc130b779c7a7c6a6a7f8
Eddie Antonio Santos <eddieantonio@hey.com>
2021-04-13 09:20:56 -0600

    feat: build languages too

--- build.js
+++ build.js
@@ -1,8 +1,15 @@
 const fs = require("fs");
 const data = require("./cached-keyman-api.json");
+const languages = require("./cached-languages.json");
 
 fs.writeFileSync(
   "./public/cached-keyman-api.json",
+  JSON.stringify(data),
+  "utf8"
+);
+
+fs.writeFileSync(
+  "./public/cached-languages.json",
   JSON.stringify(data),
   "utf8"
 );
