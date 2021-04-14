674c021765f43e0d1adbf20a4b55c58d64b54db0
Eddie Antonio Santos <eddieantonio@hey.com>
2021-04-14 10:10:53 -0600

    change: return { languages: [] } to prevent Array constructor vulnerability

--- build.js
+++ build.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 const apiData = require("./cached-keyman-api.json");
-const languages = require("./cached-languages.json");
+let languages = require("./cached-languages.json");
+
+/* Never send a bare Array, to prevent Array constructor poisoning attack.
+ * See: https://haacked.com/archive/2008/11/20/anatomy-of-a-subtle-json-vulnerability.aspx/ */
+if (languages instanceof Array) {
+  languages = { languages };
+}
 
 exportAsMinimalJSON(apiData, "cached-keyman-api.json");
 exportAsMinimalJSON(languages, "cached-languages.json");
